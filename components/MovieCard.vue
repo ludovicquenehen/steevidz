efb80f95622465cb1e6319ffa2ea352ac57b58e3
Ludovic QUENEHEN <ludovic.quenehen@placement-direct.fr>
2023-04-15 21:08:14 +0200

    movie card component

---
+++ components/MovieCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="shadow-xl hover:scale-105">
+    <div class="relative">
+      <img v-if="imgSrc" :src="imgSrc" class="w-[500px]" />
+      <div v-else class="no-img-available h-full w-full" />
+      <div class="bg-black bg-opacity-50 absolute px-6 py-2 top-0 left-0 text-white">
+        <span>{{ value.vote_average.toFixed(1) }}/10 ({{ value.vote_count }} votes)</span>
+      </div>
+      <div class="bg-black bg-opacity-50 absolute px-6 py-2 bottom-0 left-0 text-white h-1/2 space-y-2">
+        <span class="text-xl">{{ value.title }} ({{ value.release_year }})</span>
+        <span class="overview">{{ value.overview }}</span>
+        <div class="text-sm space-x-1 flex flex-wrap">
+          <span v-for="genre in value.genre_names" class="rounded-full bg-gray bg-opacity-50 py-2 px-4 mb-1">{{ genre }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+import { posterPath } from '~/api/tmdb'
+import { Movie } from '~/types/TMDB.type'
+
+@Component
+export default class MovieCard extends Vue {
+  @Prop({ type: Object, required: true })
+  value: Movie
+
+  get imgSrc() {
+    return this.value.poster_path ? `${posterPath}${this.value.poster_path}` : null
+  }
+}
+</script>
+
+<style scoped>
+.no-img-available {
+  background: repeating-linear-gradient(
+    45deg,
+    #c0c0c0,
+    #c0c0c0 10px,
+    #fff 10px,
+    #fff 20px
+  );
+  filter: blur(1px);
+}
+
+.overview {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  line-clamp: 3; 
+  -webkit-box-orient: vertical;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col">
-    <div class="flex justify-center sm:flex-row flex-col sm:space-x-4 space-x-0 sm:space-y-0 space-y-4">
+    <div class="flex justify-center sm:flex-row flex-col sm:space-x-4 space-x-0 sm:space-y-0 space-y-4 sm:px-24 px-4">
       <SearchBar
         v-model="query"
         placeholder="Rechercher un film"
@@ -9,6 +9,7 @@
       <InputNumber
         v-model="year"
         placeholder="Année"
+        @keyup.enter.native=" search"
       />
       <Button
         text="Rechercher"
@@ -16,6 +17,16 @@
         @click="search"
       />
     </div>
+    <Loader v-if="loading" class="mx-auto mt-6" />
+    <template v-else>
+      <div v-if="movies.length > 0" class="mt-6 grid sm:grid-cols-5 grid-cols-1 gap-4">
+        <MovieCard v-for="movie in movies" :key="movie.id" :value="movie" />
+      </div>
+      <Pagination v-if="movies.length > 0 && nbPages > 1 && !loading" v-model="page" :nb-pages="nbPages" class="mx-auto mt-6" @change="search(false)" />
+      <div v-if="searched && movies.length === 0 && !loading" class="mt-8 text-primary text-xl m-auto">
+        Acucun résultat pour votre recherche
+      </div>
+    </template>
   </div>
 </template>
 
@@ -24,15 +35,59 @@ import { Component, Vue } from 'nuxt-property-decorator'
 import SearchBar from '@/components/SearchBar.vue'
 import InputNumber from '~/components/InputNumber.vue'
 import Button from '@/components/Button.vue'
+import MovieCard from '@/components/MovieCard.vue'
+import Pagination from '@/components/Pagination.vue'
+import Loader from '@/components/Loader.vue'
+
+import { searchMovie, getGenres } from '~/api/tmdb'
+import { Movie, Genre } from '~/types/TMDB.type.js'
+
 @Component({
-  components: { SearchBar, InputNumber, Button }
+  components: {
+    SearchBar,
+    InputNumber,
+    Button,
+    MovieCard,
+    Pagination,
+    Loader
+  }
 })
 export default class HomePage extends Vue {
+  public loading: boolean = false
+  public searched: boolean = false
+
   public query: string =  ''
   public year: string = ''
+  public movies: Movie[] = []
 
-  search() {
+  public page: number = 1
+  public nbPages: number = 0
+
+  public genres: Genre[] = []
+
+  async search(reset = true) {
+    if (!this.query) return
+    if (reset) this.page = 1
+
+    this.loading = true
+    const { results, total_pages  } = await searchMovie(this.query, this.page, this.year)
+    this.movies = results.map((e: Movie) => ({
+      ...e,
+      genre_names: e.genre_ids.map((i: number) => this.genres.find((g: Genre) => g.id === i)?.name),
+      release_year: new Date(e.release_date).getFullYear()
+    }))
+    this.nbPages = total_pages
+    this.searched = true
     
+    // On simule un temps d'attente sinon le spinner n'est pas visible
+    setTimeout(() => {
+      this.loading = false
+    }, 1000)
+  }
+
+  async asyncData() {
+    const genres = await getGenres()
+    return { genres }
   }
 }
 </script>
