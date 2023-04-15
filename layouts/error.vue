6ab91a9187686c9e966d2cb3e9a42e54df206c8a
Ludovic QUENEHEN <ludovic.quenehen@placement-direct.fr>
2023-04-15 22:04:00 +0200

    error page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col text-center">
+    <span class="text-primary text-8xl m-auto">{{ error.statusCode }}</span>
+    <span class="text-primary text-4xl m-auto">Oups... petite erreur dans la matrice</span>
+    <span class="text-primary text-2xl m-auto">Retenter votre chance un peu plus tard</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
