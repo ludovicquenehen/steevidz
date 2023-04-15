a03545168342c8a91593df2162557eb9fb7b60a2
Ludovic QUENEHEN <ludovic.quenehen@placement-direct.fr>
2023-04-15 21:08:26 +0200

    loader component

---
+++ components/Loader.vue
@@ -0,0 +1,32 @@
+<template>
+  <span class="loader"></span>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Loader extends Vue { }
+</script>
+
+<style scoped>
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 5px solid #FFF;
+  border-bottom-color: #13AFFA;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+  }
+
+  @keyframes rotation {
+  0% {
+      transform: rotate(0deg);
+  }
+  100% {
+      transform: rotate(360deg);
+  }
+} 
+</style>
